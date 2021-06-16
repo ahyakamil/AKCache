3d058fec30f17f740e9396317b70d4f2e76ba32c
ahya kamil <ahyakamil@ahyas-MacBook-Pro.local>
2021-06-16 15:03:45 +0700

    improve key

--- src/main/java/com/kipaskipas/cache/config/AspectConfig.java
+++ src/main/java/com/kipaskipas/cache/config/AspectConfig.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 @Aspect
 public class AspectConfig {
@@ -19,11 +20,14 @@ public class AspectConfig {
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         Method method = signature.getMethod();
         String paramsKey = "";
-        for(Class cl: method.getParameterTypes()) {
-            paramsKey += cl.getName() + "_";
+        Object[] args = pjp.getArgs();
+        for(int argIndex=0; argIndex < args.length; argIndex++) {
+            Parameter parameter = method.getParameters()[argIndex];
+            paramsKey += parameter.getType().getName() + "_";
+            paramsKey += parameter.getName() + "(" + args[argIndex] + ")" + "_";
         }
-        String key = pjp.getTarget().getClass() + ":" + method.getName() + ":" + paramsKey;
-        logger.info("before method " + key + " run");
+        String key = pjp.getTarget().getClass().getName() + ":" + method.getName() + ":" + paramsKey;
+        logger.info("before method: " + key);
         String keyAnnotation = method.getAnnotation(KipaskipasCache.class).key();
         logger.info("key annotation: " + keyAnnotation);
         Object procced = pjp.proceed();
